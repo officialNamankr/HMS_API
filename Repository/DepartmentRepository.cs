31f5d120765981eb9be99950c4a2e891e3411113
Piyush Kumar <51694158+piyush2040@users.noreply.github.com>
2023-03-28 17:33:42 +0530

    Updated department repository

--- Repository/DepartmentRepository.cs
+++ Repository/DepartmentRepository.cs
@@ -66,26 +66,27 @@ namespace HMS_API.Repository
         public async Task<List<DoctorByDepartmentViewDTO>> GetAllDoctorsByDeptId(Guid id)
         {
             //var depts = await _db.Departments.Where(u => u.Id.Equals(id)).Include(Doctor => Doctor.Id).Include(Doctor => Doctor.Name).ToListAsync();
-            var depts = await _db.Departments.Where(u => u.Id.Equals(id)).Include(Department => Department.Doctors).ToListAsync();
+            var depts = await _db.Departments.Where(u => u.Id.Equals(id)).Include(Department => Department.Doctors).FirstOrDefaultAsync();
             
 
             List<DoctorByDepartmentViewDTO> departments = new List<DoctorByDepartmentViewDTO>();
             
-            foreach (var dept in depts)
-            {
-                var doctorId = "";
-                foreach (var doctor in dept.Doctors)
+            
+                //var doctorId = "";
+                foreach (var doctor in depts.Doctors)
                 {
                    var doctorUser = await _db.Doctors.Where(u => u.DoctorId.Equals(doctor.DoctorId)).FirstOrDefaultAsync();
-                   doctorId = doctorUser.DoctorId;
-
+                   
+                   var doc = await _db.Doctors.Where(u => u.DoctorId.Equals(doctorUser.DoctorId)).Include(Doctor => Doctor.User).FirstOrDefaultAsync();
+                    var dep = new DoctorByDepartmentViewDTO { DoctorId = doc.DoctorId, DoctorName = doc.User.Name };
+                    departments.Add(dep);
                 }
-                var doc = await _db.Doctors.Where(u => u.DoctorId.Equals(doctorId)).Include(Doctor => Doctor.User).FirstOrDefaultAsync();
-                if (doc == null)
-                    return departments;
-                var dep = new DoctorByDepartmentViewDTO { DoctorId = doc.DoctorId, DoctorName = doc.User.Name };
-                departments.Add(dep);
-            }
+                //var doc = await _db.Doctors.Where(u => u.DoctorId.Equals(doctorId)).Include(Doctor => Doctor.User).FirstOrDefaultAsync();
+                //if (doc == null)
+                    //return departments;
+                //var dep = new DoctorByDepartmentViewDTO { DoctorId = doc.DoctorId, DoctorName = doc.User.Name };
+                //departments.Add(dep);
+            
             return departments;
         }
         public async Task<bool> DeleteDepartment(Guid id)
